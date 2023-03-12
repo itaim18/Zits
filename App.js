67e74e34df5128a28e1c3ae58356629b6e526adc
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-03-12 16:50:38 +0200

    👨‍💻  modifications

--- App.js
+++ App.js
@@ -2,9 +2,18 @@ import * as React from "react";
 import { Button, Text, TextInput, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from "recoil";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-const AuthContext = React.createContext();
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+import { app } from "./firebaseConfig";
+
 function Feed() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -14,7 +23,6 @@ function Feed() {
 }
 
 function Profile() {
-  const { signOut } = React.useContext(AuthContext);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Signed in!</Text>
@@ -86,132 +94,60 @@ function HomeScreen() {
   return <MyTabs />;
 }
 
-function SignInScreen() {
-  const [username, setUsername] = React.useState("");
+function LoginScreen() {
+  const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-  const { signIn } = React.useContext(AuthContext);
+  const auth = getAuth(app);
 
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log("Signed In!");
+      })
+      .catch((error) => console.log(error));
+  };
+  const handleCreateAccount = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log("Account created!");
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <View>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
+      <TextInput placeholder="email" value={email} onChangeText={setEmail} />
       <TextInput
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title="Sign in" onPress={() => signIn({ username, password })} />
+      <Button title="Sign in" onPress={handleSignIn} />
+      <Button title="Register" onPress={handleCreateAccount} />
     </View>
   );
 }
 
 const Stack = createNativeStackNavigator();
 
-export default function App({ navigation }) {
-  const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case "RESTORE_TOKEN":
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case "SIGN_IN":
-          return {
-            ...prevState,
-            isSignout: false,
-            userToken: action.token,
-          };
-        case "SIGN_OUT":
-          return {
-            ...prevState,
-            isSignout: true,
-            userToken: null,
-          };
-      }
-    },
-    {
-      isLoading: true,
-      isSignout: false,
-      userToken: null,
-    }
-  );
-
-  React.useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
-    const bootstrapAsync = async () => {
-      let userToken;
-
-      try {
-        // Restore token stored in `SecureStore` or any other encrypted storage
-        // userToken = await SecureStore.getItemAsync('userToken');
-      } catch (e) {
-        // Restoring token failed
-      }
-
-      // After restoring token, we may need to validate it in production apps
-
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      dispatch({ type: "RESTORE_TOKEN", token: userToken });
-    };
-
-    bootstrapAsync();
-  }, []);
-
-  const authContext = React.useMemo(
-    () => ({
-      signIn: async (data) => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
-      },
-      signOut: () => dispatch({ type: "SIGN_OUT" }),
-      signUp: async (data) => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
-      },
-    }),
-    []
-  );
-
+export default function App() {
   return (
-    <AuthContext.Provider value={authContext}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {state.isLoading ? (
-            // We haven't finished checking for the token yet
-            <Stack.Screen name="Splash" component={SplashScreen} />
-          ) : state.userToken == null ? (
-            // No token found, user isn't signed in
+    <SafeAreaProvider>
+      <RecoilRoot>
+        <NavigationContainer>
+          <Stack.Navigator>
             <Stack.Screen
-              name="SignIn"
-              component={SignInScreen}
-              options={{
-                title: "Sign in",
-                // When logging out, a pop animation feels intuitive
-                animationTypeForReplace: state.isSignout ? "pop" : "push",
-              }}
+              options={{ headerShown: false }}
+              name="Login"
+              component={LoginScreen}
             />
-          ) : (
-            // User is signed in
             <Stack.Screen name="Home" component={HomeScreen} />
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthContext.Provider>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </RecoilRoot>
+    </SafeAreaProvider>
   );
 }
